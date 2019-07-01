3e5a23e9866a94c4666c9358912edaa39f80bf1b
robinganemccalla@gmail.com <robinganemccalla@gmail.com>
2019-07-01 15:05:34 -0400

    Fix canvas and onclicks

--- script.js
+++ script.js
@@ -9,25 +9,28 @@ function toggle(){
     }
 }
 
-var margin = {top: 30, right: 50, bottom: 30, left: 50}
-var canvas_width = 700 - margin.right -margin.left;
-var canvas_height = 700 -margin.top - margin.bottom;
+//var margin = {top: 3%; right: 5%; bottom: 3%; left: 5%}
+// var canvas_width = 70%;// - margin.right -margin.left;
+// var canvas_height = 70%;// -margin.top - margin.bottom;
 var graph = d3.select("#graph")
     .append("svg:svg")
-        .attr("width", canvas_width + margin.left + margin.right)
-        .attr("height", canvas_height + margin.top + margin.bottom)
+         .attr("width", "35em")
+         .attr("height", "35em")
+         .attr("id", "svgholder")
     .append("circle")
         .attr("id", 'blueCircle')
-        .attr("x",200)
-        .attr("y",200)
-        .attr("r", 250)
+        // .attr("x",20em)
+        // .attr("y",20%)
+        .attr("r", document.getElementById('svgholder').width.baseVal['value']/2)
         .attr("fill","blue")
-        .attr("cx",400)
-        .attr("cy",400)
+        .attr("cx",document.getElementById('svgholder').width.baseVal['value']/2)
+        .attr("cy",document.getElementById('svgholder').width.baseVal['value']/2)
         .attr("onclick", "toggle()")
 
 var code = d3.select("#code")
     .append("text")
         .attr("id", 'codeForBlueCircle')
         .attr("onclick", "toggle()")
+        .attr("width", "35em")
+        .attr("height", "35em")
         .text("<circle id=\"blueCircle\" x=\"200\" y=\"200\" r=\"250\" fill=\"blue\" cx=\"400\" cy=\"400\"></circle>")
