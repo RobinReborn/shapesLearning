6df70fc9eb3a73626ecf5b6305b0ff17dd649698
robinganemccalla@gmail.com <robinganemccalla@gmail.com>
2019-07-05 14:29:59 -0400

    Have text in circle scaling to size, not perfectly centered but good

--- my-app/src/Circle.jsx
+++ my-app/src/Circle.jsx
@@ -5,7 +5,7 @@ class Circle extends React.Component{
 	render() {
 		return (			
 			<svg viewBox="-50 -50 100 100">
-				<circle id="blueCircle" r="50" fill="blue" cx="0" cy="0"/>
+				<circle r="50" fill="blue"/>
 			</svg>
 		)
 	}
--- my-app/src/ClickCircle.jsx
+++ my-app/src/ClickCircle.jsx
@@ -20,7 +20,7 @@ class ClickCircle extends React.Component{
 			<div className='card' style={{opacity:this.state.opacity, zIndex:this.state.zIndex}}>
 				<Circle />
 			</div>
-			<div id='code' className='code card'>
+			{/*<div className='code'>
 	            <br/>
 	            &lt;circle
 	            <br/>
@@ -29,7 +29,26 @@ class ClickCircle extends React.Component{
 	            fill={this.state.fill}&gt;
 	            <br/>
 	            &lt;/circle&gt;
-	        </div>
+	        </div>*/}
+	        <div className='card'>
+	        <svg viewBox="0 0 100 100">
+  			<text x="30" y="25" fontSize="12" fill="black">&lt;circle</text>
+   			<text x="35" y="45" fontSize="12" fill="black">r={this.state.r}</text>
+  			<text x="28" y="65" fontSize="12" fill="black">fill={this.state.fill}&gt;</text>
+  			<text x="29" y="85" fontSize="12" fill="black">&lt;/circle&gt;</text>
+
+{/*
+<br/>
+	            &lt;circle
+	            <br/>
+	            r={this.state.r}
+	            <br/>
+	            fill={this.state.fill}&gt;
+	            <br/>
+	            &lt;/circle&gt;  		*/}
+			</svg>
+			</div> 
+
 		</div>
 		)
 	}
