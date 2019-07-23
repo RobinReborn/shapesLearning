5e7402b915ea2e9d961f07b741d9d0ea0bd14d35
robinganemccalla@gmail.com <robinganemccalla@gmail.com>
2019-07-23 17:01:54 -0400

    more advanced tests

--- src/tests/basic.test.js
+++ src/tests/basic.test.js
@@ -8,19 +8,33 @@ import { expect } from 'chai';
 import { mount } from 'enzyme';
 import sinon from 'sinon';
 
-import ShowShape from '../ShowShape.jsx'
-import Instructions from '../Instructions.jsx'
+import ShowShape from '../ShowShape'
+import Instructions from '../Instructions'
 import { Provider } from 'react-redux';
 import rootReducer from '../reducers'
 import { createStore } from 'redux';
-import Layout from '../Layout'
+import Layout from '../Layout';
+import ClickCircle from '../Shapes/ClickCircle';
+import ClickRectangle from '../Shapes/ClickRectangle';
+
 describe('<ShowShape/>', () => {
 	const store = createStore(rootReducer)
 
 	it('has an <Instructions> component', () => {
 		const wrapper = mount(<Provider store={store}> <ShowShape/></Provider>);
-		console.log(wrapper)
 		expect(wrapper.find(Layout)).to.have.lengthOf(1)
 		expect(wrapper.find(Instructions)).to.have.lengthOf(1)
+		expect(wrapper.find(ClickCircle)).to.have.lengthOf(1)
+
+	})
+	it("clicking gets to next shape", () => {
+		const onButtonClick = sinon.spy();
+		const wrapper = mount(<Provider store={store}> <ShowShape/></Provider> )
+		wrapper.find('.flipHolder').simulate('click')
+		expect(wrapper.find(Instructions).find('#instructions').text()).to.match(/^Click again to see the next shape/)
+		wrapper.find('.flipHolder').simulate('click')
+		expect(wrapper.find(ClickRectangle)).to.have.lengthOf(1)
+		expect(wrapper.find(Instructions).find('#instructions').text()).to.match(/^Click on the Rectangle on the left to see the code that generates it/)
+
 	})
 })
