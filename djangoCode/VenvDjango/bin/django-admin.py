3987239b0abc99eeeb157092d18e3df4e7a64874
robinganemccalla@gmail.com <robinganemccalla@gmail.com>
2019-08-21 17:32:47 -0400

    Basic begining of configuration

---
+++ djangoCode/VenvDjango/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/robin/workspace/shape-learning/djangoCode/VenvDjango/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
